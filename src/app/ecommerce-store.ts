f603105f08b2d0f1f6ac3bf653abf5dfc7e7b667
aldocaava <aldocaava@hotmail.com>
2026-02-19 20:48:25 -0500

    added NGRX store with signal events

---
+++ src/app/ecommerce-store.ts
@@ -0,0 +1,90 @@
+import { patchState, signalMethod, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { Product } from './models/product';
+import { computed } from '@angular/core';
+
+export type EcommerceState = {
+    products: Product[];
+    category: string;
+    wishlistItems: Product[];
+}
+
+const initialState: EcommerceState = {
+    products: [
+            {
+                id: '1',
+                name: 'Product 1',
+                description: 'Product 1 description',
+                price: 10,
+                imageUrl: 'https://picsum.photos/200/300',
+                rating: 4,
+                reviewCount: 10,
+                category: 'Electronics',
+                inStock: true,
+                },
+                {
+                id: '2',
+                name: 'Product 2',
+                description: 'Product 2 description',
+                price: 20,
+                imageUrl: 'https://picsum.photos/200/300',
+                rating: 3,
+                reviewCount: 5,
+                category: 'Electronics',
+                inStock: true,
+                },
+                {
+                id: '3',
+                name: 'Product 3',
+                description: 'Product 3 description',
+                price: 30,
+                imageUrl: 'https://picsum.photos/200/300',
+                rating: 5,
+                reviewCount: 15,
+                category: 'Electronics',
+                inStock: false,
+                },
+                {
+                id: '4',
+                name: 'Product 4',
+                description: 'Product 4 description',
+                price: 40,
+                imageUrl: 'https://picsum.photos/200/300',
+                rating: 4,
+                reviewCount: 20,
+                category: 'Home',
+                inStock: true,
+                },
+        ],
+    category: 'all',
+    wishlistItems: [],
+}
+
+export const EcommerceStore = signalStore(
+    {
+        providedIn: 'root', //make the state globally available
+    },
+    withState(initialState),
+    withComputed((state) => ({
+        
+        filteredProducts: computed(() => {
+            console.log(state.category());
+          if (state.category() === 'all') {
+                return state.products();
+            } else{
+                return state.products().filter(product => product.category.toLowerCase() === state.category().toLowerCase());
+            }  
+          
+        })
+    
+    })),
+    withMethods((store) => ({
+       setCategory: signalMethod<string>((category: string) => {
+            patchState(store, {category});
+        }),
+        addToWishlist: signalMethod<Product>((product: Product) => {
+           patchState(store, {wishlistItems: [...store.wishlistItems(), product]});
+           alert('Product added to wishlist');
+        }),
+    }))
+  
+)
--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,11 +1,11 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCard } from '../../components/product-card/product-card';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { MatNavList, MatListItem, MatListItemTitle } from '@angular/material/list';
-import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from "@angular/router";
 import { TitleCasePipe } from '@angular/common';
+import { EcommerceState, EcommerceStore } from '../../ecommerce-store';
 
 @Component({
   selector: 'app-products-grid',
@@ -13,72 +13,29 @@ import { TitleCasePipe } from '@angular/common';
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
-export class ProductsGrid {
-  products = signal<Product[]>([
-    {
-      id: '1',
-      name: 'Product 1',
-      description: 'Product 1 description',
-      price: 10,
-      imageUrl: 'https://picsum.photos/200/300',
-      rating: 4,
-      reviewCount: 10,
-      category: 'Electronics',
-      inStock: true,
-    },
-    {
-      id: '2',
-      name: 'Product 2',
-      description: 'Product 2 description',
-      price: 20,
-      imageUrl: 'https://picsum.photos/200/300',
-      rating: 3,
-      reviewCount: 5,
-      category: 'Electronics',
-      inStock: true,
-    },
-    {
-      id: '3',
-      name: 'Product 3',
-      description: 'Product 3 description',
-      price: 30,
-      imageUrl: 'https://picsum.photos/200/300',
-      rating: 5,
-      reviewCount: 15,
-      category: 'Electronics',
-      inStock: false,
-    },
-    {
-      id: '4',
-      name: 'Product 4',
-      description: 'Product 4 description',
-      price: 40,
-      imageUrl: 'https://picsum.photos/200/300',
-      rating: 4,
-      reviewCount: 20,
-      category: 'Home',
-      inStock: true,
-    },
-  ]);
-  
+export class ProductsGrid implements OnInit{
+ 
+  constructor() {
+    //change the category in the store to trigger the computed property
+    //will listen the signal updates
+    this.store.setCategory(this.category);
+    
+   }
+
+
+  public store = inject(EcommerceStore);
+ 
   //uses withComponentInputBinding() to access the route params
   public categories = signal<string[]>(['all', 'electronics', 'home', 'clothes', 'accessories']);
   public category = input<string>('all');
   
-
-  public filteredProducts = computed(() => {
-    if (this.category() === 'all') {
-      return this.products();
-    } else{
-      return this.products().filter(product => product.category.toLowerCase() === this.category().toLowerCase());
-    }    
-  });
-
   public addToCart(product: Product) {
     console.log(product);
   }
 
-  public showCategory(category: string) {
-    console.log(category);
+  
+  
+  ngOnInit(): void {
+      console.log(this.store);
   }
 }
